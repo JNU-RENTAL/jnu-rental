3c36e373c02d3913986dde4e67fceff674dee005
kimmjae99 <kimmjae99@gmail.com>
2023-05-29 14:50:42 +0900

    프로필 화면 구현

--- routes/page.js
+++ routes/page.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
-const { User } = require("../models");
+const { User, Recruitment, Place} = require("../models");
 
 const router = express.Router();
 
-router.get("/profile", isLoggedIn, (req, res) => {
-  res.render("profile", { title: "내 정보" });
+router.get("/profile", isLoggedIn, async (req, res) => {
+  const places = await Place.findAll({});
+  const user = req.user;
+  const recruit = await Recruitment.findAll({
+    include: [
+      {
+        model: Place,
+        required: true,
+      },
+    ],
+    where: {user_id: user.id},
+  });
+
+  try {
+    res.render("profile", { title: `내 정보`, user, recruits : recruit });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // id에 해당하는 게시물 조회
+    const recruit = await Recruitment.findByPk(id);
+    
+    if (!recruit) {
+      // 해당 게시물이 존재하지 않는 경우 404 에러 응답
+      res.status(404).send("페이지를 찾을 수 없습니다");
+      return;
+    }
+    // recruit.pug 파일 렌더링
+    res.render("recruit_post", { title: "Recruit_post", user: res.user, post: recruit });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
+  }
 });
 
 router.get("/join", isNotLoggedIn, (req, res) => {
@@ -26,10 +63,6 @@ router.get("/select", (req, res) => {
 
 router.get("/sendEmail", (req, res) => {
   res.render("sendEmail", { title: "sendEmail"});
-})
-
-router.get("/profile", (req, res) => {
-  res.render("profile", { title: "profile"});
 })
 
 module.exports = router;
