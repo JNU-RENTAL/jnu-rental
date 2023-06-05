59c4ad1ff57722e6159dc73bffb70ef3bef32429
Mun JunHyeok <jjhh0919@gmail.com>
2023-06-05 19:27:21 +0900

    글 수정하기 기능 추가

--- routes/recruit.js
+++ routes/recruit.js
@@ -7,7 +7,7 @@ const sequelize = require("sequelize");
 const router = express.Router();
 router.get("/write", isLoggedIn, async (req, res) => {
   const places = await Place.findAll({});
-  res.render("recruit_write", { title: "모집 글 작성", places: places });
+  res.render("recruit_write", { title: "모집 글 작성", places });
 });
 
 router.post("/write", isLoggedIn, async (req, res, next) => {
@@ -83,11 +83,25 @@ router.get("/:place", isLoggedIn, async (req, res) => {
       order: sequelize.col("createdAt"),
     });
 
+    const comments = await Comment.findAll({
+      attributes: [
+        "recruitment_id",
+        [sequelize.fn("COUNT", sequelize.col("recruitment_id")), "count"],
+      ],
+      group: ["recruitment_id"],
+    });
+
+    const commentsArray = comments.map((comment) => ({
+      postId: comment.recruitment_id,
+      count: comment.get("count"),
+    }));
+
     res.render("recruit", {
       title: `${req.params.place} 모집`,
-      places: places,
+      places,
       posts: req.query.q ? searchPosts : posts,
       user: req.user,
+      comments: commentsArray,
     });
   } catch (err) {
     console.error(err);
@@ -130,6 +144,51 @@ router.post(
     }
   }
 );
+
+router.get("/edit/post/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // id에 해당하는 게시물 조회
+    const recruit = await Recruitment.findByPk(id);
+    const places = await Place.findAll({});
+    const nowPlace = await Place.findByPk(recruit.place);
+
+    // recruit.pug 파일 렌더링
+    res.render("recruit_write", {
+      title: "모집 글 수정",
+      post: recruit,
+      places,
+      nowPlace,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
+  }
+});
+
+router.post("/edit/:post_id", async (req, res, next) => {
+  const { write_place, write_title, write_context } = req.body;
+  const place = await Place.findAll({
+    where: {
+      name: write_place,
+    },
+  });
+  try {
+    await Recruitment.update(
+      {
+        title: write_title,
+        text: write_context,
+        place: place[0].id,
+      },
+      { where: { id: req.params.post_id } }
+    );
+    return res.redirect(`/recruit/post/${req.params.post_id}`);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
 router.get("/post/:id", async (req, res) => {
   const { id } = req.params;
