94f3c3c65b5110493ceb6aff981cab1625d07c8a
kimmjae99 <kimmjae99@gmail.com>
2023-06-06 13:01:36 +0900

    profile 페이지 머지 완료 및 예약정보입력페이지 UI수정

--- routes/page.js
+++ routes/page.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
-const { User, Place, Recruitment } = require("../models");
-const { cat } = require("require/example/shared/dependency");
+const { User, Recruitment, Place} = require("../models");
+const Reservation = require("../models/reservation");
 
 const router = express.Router();
 
-router.get("/profile", isLoggedIn, (req, res) => {
-  res.render("profile", { title: "내 정보" });
+router.get("/profile", isLoggedIn, async (req, res) => {
+  const places = await Place.findAll({});
+  const user = req.user;
+  const reservation = await Reservation.findAll({
+    include: [
+      {
+        model: Place,
+        required: true,
+      },
+    ],
+    where:{user_id:user.id}
+  });
+
+  try {
+    res.render("profile", { title: `내 정보`, user, reservations : reservation });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // id에 해당하는 게시물 조회
+    const recruit = await Recruitment.findByPk(id);
+    
+    if (!recruit) {
+      // 해당 게시물이 존재하지 않는 경우 404 에러 응답
+      res.status(404).send("페이지를 찾을 수 없습니다");
+      return;
+    }
+    // recruit.pug 파일 렌더링
+    res.render("recruit_post", { title: "Recruit_post", user: res.user, post: recruit });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
+  }
 });
 
 router.get("/join", isNotLoggedIn, (req, res) => {
@@ -43,6 +80,8 @@ router.get("/apply/:place/:date/:time", isLoggedIn, (req, res) => {
     time: req.params.time,
   });
 });
+
+
 router.get("/", (req, res, next) => {
   res.render("main", { title: "Main", user: req.user });
 });
