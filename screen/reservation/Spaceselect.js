091003318a6e6b1b1e9cbfb831687fbd26c57025
Mun JunHyeok <jjhh0919@gmail.com>
2023-06-12 18:18:15 +0900

    screen 삭제

--- screen/reservation/Spaceselect.js
+++
@@ -1,8 +0,0 @@
-const btns = document.querySelectorAll('.space_btn button');
-      btns.forEach((btn) => {
-      btn.addEventListener('click', () => {
-        const spaceName = btn.textContent;
-        const url = `Dateselect.html?space=${encodeURIComponent(spaceName)}`; // 쿼리 파라미터 추가
-      location.href = url;
-      });
-    });
