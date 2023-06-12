07dc8e969fe4fb793f75dfba92adfa7ae0a3e614
Mun JunHyeok <jjhh0919@gmail.com>
2023-06-12 17:40:57 +0900

    프로필 화면 경로 수정

--- routes/page.js
+++ routes/page.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
-const { User, Recruitment, Place} = require("../models");
+const { User, Recruitment, Place } = require("../models");
 const Reservation = require("../models/reservation");
 
 const router = express.Router();
 
-router.get("/profile", isLoggedIn, async (req, res) => {
+router.get("/profile/:user_id", async (req, res) => {
   const places = await Place.findAll({});
-  const user = req.user;
+  const user = req.params.user_id;
   const reservation = await Reservation.findAll({
     include: [
       {
@@ -15,11 +15,15 @@ router.get("/profile", isLoggedIn, async (req, res) => {
         required: true,
       },
     ],
-    where:{user_id:user.id}
+    where: { user_id: user },
   });
 
   try {
-    res.render("profile", { title: `내 정보`, user, reservations : reservation });
+    res.render("profile", {
+      title: `내 정보`,
+      user,
+      reservations: reservation,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
@@ -32,14 +36,18 @@ router.get("/post/:id", async (req, res) => {
   try {
     // id에 해당하는 게시물 조회
     const recruit = await Recruitment.findByPk(id);
-    
+
     if (!recruit) {
       // 해당 게시물이 존재하지 않는 경우 404 에러 응답
       res.status(404).send("페이지를 찾을 수 없습니다");
       return;
     }
     // recruit.pug 파일 렌더링
-    res.render("recruit_post", { title: "Recruit_post", user: res.user, post: recruit });
+    res.render("recruit_post", {
+      title: "Recruit_post",
+      user: res.user,
+      post: recruit,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("서버 에러"); // 에러 발생 시 500 에러 응답
@@ -81,17 +89,16 @@ router.get("/apply/:place/:date/:time", isLoggedIn, (req, res) => {
   });
 });
 
+router.get("/select", (req, res) => {
+  res.render("select", { title: "Select", user: req.user });
+});
+
+router.get("/sendEmail", (req, res) => {
+  res.render("sendEmail", { title: "sendEmail" });
+});
 
 router.get("/", (req, res, next) => {
   res.render("main", { title: "Main", user: req.user });
 });
-
-router.get("/select", (req, res) => {
-  res.render("select", { title: "Select"});
-});
-
-router.get("/sendEmail", (req, res) => {
-  res.render("sendEmail", { title: "sendEmail"});
-})
 
 module.exports = router;
